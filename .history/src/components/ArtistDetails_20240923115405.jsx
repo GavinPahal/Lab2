42c22dac04004a4ce1e7648c2f7bdea1744d496a
gavinPahal <gpahal4@my.bcit.ca>
2024-09-23 12:01:49 -0700

    New push

--- .history/src/components/ArtistDetails_20240923115405.jsx
+++ .history/src/components/ArtistDetails_20240923115405.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ArtistDetails = ({ artist }) => {
   return (
     <div className="artist-card">
-      <img src={artist.photo_url} alt={artist.name} style={{ width: '180px', height: '180px' }} />
+      <img src={artist.photo_url} alt={artist.name} style={{ width: '100px', height: '100px' }} />
       <h3>{artist.name}</h3>
       <p><strong>Country:</strong> {artist.country}</p>
       <p><strong>Years Active:</strong> {artist.years_active}</p>
