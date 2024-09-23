96cfa1131920e89e5828b08e11c31f178490f143
gavinPahal <gpahal4@my.bcit.ca>
2024-09-23 10:40:41 -0700

    Gavin

---
+++ component/ArtistDetails.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ArtistDetails = ({ artist }) => {
+  return (
+    <div className="artist-card">
+      <h3>{artist.name}</h3>
+      <p><strong>Genre:</strong> {artist.genre}</p>
+      <p><strong>Sales:</strong> {artist.sales} million</p>
+    </div>
+  );
+};
+
+export default ArtistDetails;
